d53ed6e27f47ea74685b06e800dcf1b72673f91d
thedakking01 <sourabhsinghbais52@gmail.com>
2025-03-19 19:30:13 +0530

    initial SETUP

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main>
+      <h1 className='text-5xl text-orange-600 font-bold'>Welcome to Metaverse</h1>
+    </main>
+  )
+}
+
+export default App
