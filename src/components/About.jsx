611ce492b4239aeab0560b96c5329f519ffa0fe6
thedakking01 <sourabhsinghbais52@gmail.com>
2025-03-21 12:33:33 +0530

    add animated titles for about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,7 @@
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/all";
-
-// import AnimatedTitle from "./AnimatedTitle";
+import AnimatedTitle from "./AnimatedTitle";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -33,10 +32,10 @@ const About = () => {
             Welcome to Project X
             </p>
 
-            {/* <AnimatedTitle
+            <AnimatedTitle
             title="Disc<b>o</b>ver the world's <br /> largest shared <b>a</b>dventure"
             containerClass="mt-5 !text-black text-center"
-            /> */}
+            />
 
             <div className="about-subtext">
             <p>The Game of Games begins—your life, now an epic MMORPG</p>
